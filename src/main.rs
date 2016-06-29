06e30850ac15c41e0bc719bd28a68cacde2c688c
Clement Miao <clementmiao@gmail.com>
2016-06-28 23:04:57 -0700

    added hostname handling for GAE

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #[macro_use] extern crate nickel;
 
+use std::process::Command;
+use std::net::SocketAddrV4;
+use std::net::Ipv4Addr;
+use std::str;
 use std::collections::HashMap;
 use nickel::{Nickel, HttpRouter,StaticFilesHandler};
 
@@ -14,5 +18,31 @@ fn main() {
 
     server.utilize(StaticFilesHandler::new("assets/css/"));
 
-    server.listen("127.0.0.1:8080");
+    let host_port = 8080; //Note: port must be 8080 for GAE
+    let hostname_cmd = Command::new("hostname").arg("-I").output();
+    let host_addr: SocketAddrV4 = match hostname_cmd {
+        Ok(res) => {
+            let addr = str::from_utf8(res.stdout.as_slice())
+                .map_err(|err| err.to_string())
+                .and_then(|ip_str| ip_str.trim()
+                            .parse::<Ipv4Addr>()
+                            .map_err(|err| err.to_string()))
+                .map(|ip| SocketAddrV4::new(ip, host_port));
+
+            match addr {
+                Ok(addr) => addr,
+                Err(_) => {
+                    let ip = Ipv4Addr::new(127, 0, 0, 1);
+                    SocketAddrV4::new(ip, host_port)
+                }
+            }
+        },
+        Err(_) => {
+            let ip = Ipv4Addr::new(127, 0, 0, 1);
+            SocketAddrV4::new(ip, host_port)
+        }
+    };
+    println!("Server listening at {}", host_addr);
+
+    server.listen(host_addr);
 }
